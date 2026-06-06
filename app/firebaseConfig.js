eede026d604f629aaa4ef7316e7f6081569e6e3e
jt1118 <jaeltung2005@gmail.com>
2026-06-07 01:47:53 +0800

    Update firebaseConfig.js

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -1,5 +1,5 @@
 // firebaseConfig.js
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 // 貼上你在 Firebase 控制台拿到的金鑰
@@ -7,8 +7,14 @@ const firebaseConfig = {
   
 };
 
-// 初始化 Firebase
-const app = initializeApp(firebaseConfig);
+let app;
+if (getApps().length === 0) {
+  // 如果目前陣列裡沒有任何 Firebase App，才初始化一個新的
+  app = initializeApp(firebaseConfig);
+} else {
+  // 如果已經有了，就直接抓現成的來用，不要重複建立
+  app = getApp();
+}
 
 // 初始化並導出 Firestore 資料庫，讓你可以在其他檔案中使用
 export const db = getFirestore(app);
